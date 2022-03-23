e976a166d22c5f6a271b1031f403d94cdb2a78da
Jeremy O'Daniel <jeremy.odaniel@gmail.com>
2022-03-23 15:47:36 -0500

    Complete webpack config & remove unnecessary files

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const webpack = require('webpack');
+
 
 
 module.exports = {
@@ -7,5 +9,11 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.bundle.js'
   },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jquery: 'jquery'
+    }),
+  ],
   mode: 'development'
 };
